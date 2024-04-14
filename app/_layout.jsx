8ccc87dd8effdb3df13e1300ee58d96649b5826b
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-04-14 18:50:51 -0300

    2: add tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,38 @@
+import { useFonts } from "expo-font";
+import React, { useEffect } from "react";
+import { SplashScreen, Stack } from "expo-router";
 
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
-}
+};
 
 export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View className="flex-1 bg-slate-900 items-center justify-center">
-      <Text className="text-emerald-500">Indux</Text>
+      <Text className="text-emerald-500 font-pblack">Indux</Text>
+      <Link href="/home" className="text-white">Go To</Link>
       <StatusBar style="auto" />
     </View>
   );
